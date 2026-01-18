9a3875e5ff7a149c5c013d230994899ef464e493
HrakDovlatyan <hrak.dovlatyan.03@gmail.com>
2026-01-18 19:41:03 +0400

    HW_GameCycle_DI

---
+++ ShootEmUp_DependencyInjection/Assets/Scripts/Common/EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace ShootEmUp {
+    public sealed class EntryPoint : MonoBehaviour {
+        [SerializeField] private UIManager _uIManager;
+
+        private GameMediator _gameMediator;
+
+        [Inject]
+        public void Construct(GameMediator gameMediator) {
+            _gameMediator = gameMediator;
+        }
+
+        private void Start() {
+            _uIManager.Init();
+            _gameMediator.Init(_uIManager);
+        }
+    }
+}
+
+
---
+++ ShootEmUp_DependencyInjection/Assets/Scripts/Installers/GameplayInstaller.cs
@@ -0,0 +1,23 @@
+﻿using ShootEmUp;
+using Zenject;
+
+public sealed class GameplayInstaller : MonoInstaller {
+
+    public override void InstallBindings() {
+        BindGameCycle();
+        BindGameMediator();
+        BindGameCycleInstaller();
+    }
+
+    private void BindGameCycle() {
+        Container.BindInterfacesAndSelfTo<GameCycle>().AsSingle();
+    }
+
+    private void BindGameMediator() {
+        Container.Bind<GameMediator>().AsSingle();
+    }
+
+    private void BindGameCycleInstaller() {
+        Container.Bind<GameCycleInstaller>().AsSingle().NonLazy();
+    }
+}
